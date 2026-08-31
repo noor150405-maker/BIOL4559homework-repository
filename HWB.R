92c29d37a3606832dcdf42dc3fdb8ef198d060e7
Noor_Khan <noor150405@gmail.com>
2026-08-30 23:08:29 -0400

    Update Hardy Weinberg Grapgh

--- HWB.R
+++ HWB.R
@@ -1,4 +1,4 @@
-### install packages; you only need to this once (ever)
+pw### install packages; you only need to this once (ever)
 install.packages("ggplot2", "data.table")
 
 yes
@@ -6,62 +6,44 @@ yes
 library(ggplot2)
 library(data.table)
 
-### function
-test_fun <- function(x, y) {
-  return(x*y)
-}
-
-test_fun2 <- function(x, y) {
-  z <- q^2
-  return(data.table(x=p^2, y=2pq, z=q^2))
-}
-
-test_fun3 <- function(x,y,z) {
-  x <- p^2
-  y <- 2pq
-  z <- q^2
+### Hardy-Weinberg function
+HWB_fun <- function(p) {
+  q <- 1 - p
   
+  FAA <- p^2
+  FAa <- 2*p*q
+  Faa <- q^2
   
-  return(data.table(x=x, y=y, z=z))
+  return(data.table(p=p, FAA=FAA, FAa=FAa, Faa=Faa))
 }
 
 ### generate data
-### this function returns a single value
-test_fun(x=5, y=2)
+out <- HWB_fun(p=seq(0, 1, by=0.01))
 
-### the same function can return a vector of values.
-test_fun(x=c(1:10), y=c(-1:-10))
+### convert wide to long
+out_long <- melt(
+  out,
+  id.vars="p",
+  measure.vars=c("FAA", "FAa", "Faa")
+)
 
-### does this work? What does the error message mean? What happens if you change the length of the vectors to be multiples of each other?
-test_fun(x=c(1:10), y=c(1:3))
+### graph
+p1 <- ggplot(data=out_long) +
+  geom_line(aes(x=p, y=value, group=variable, color=variable)) +
+  labs(
+    x="p",
+    y="value",
+    color="variable",
+    title="Hardy-Weinberg Equilibrium"
+  ) +
+  theme_classic()
 
-### the output of this function helps you keep track of input and output by returning a data.table
-out <- test_fun2(x=c(0:1), y=c(-1:-10))
-ggplot(data=out, aes(x=x, y=y, color=z)) + geom_point()
+### graph
+p1
 
-### there are a few basic data types in R:
-a <- "alan" ### this is vector of length 1
-b <- c("alan", "robert") ### this is a vector of length 2. We need to use the "c" function to concatenate the two single values
-c <- data.table(name=c("alan", "robert"), role=c("teacher", "TA"), number=c(2,1)) ### this is a data.table/data.frame. It is a 2 dimensional array, with two columns and two rows. Each column can take a different type of data, like a character string, a number, etc
+### graph
+ggsave(p1, file="~/Hardy_Weinberg_plot.pdf")
 
-### This function performs two operations on the input value and returns a WIDE table. We can plot y1 and y2 on the same graph in a few differnet ways
-out2 <- test_fun3(x=c(0:1))
+getwd()
+list.files()
 
-### method 1: manually code the two y-values.
-ggplot(data=out2) +
-  geom_line(aes(x=x, y=y1), color="blue") +
-  geom_line(aes(x=x, y=y2), color="orange")
-
-### convert wide to long and use groupings in ggplot
-out2_long <- melt(out2, id.vars="x", measure.vars=c("y1", "y2"))
-
-ggplot(data=out2_long) +
-  geom_line(aes(x=x, y=value, group=variable, color=variable))
-
-### saving plots is easy
-p1 <- ggplot(data=out2_long) +
-  geom_line(aes(x=x, y=value, group=variable, color=variable))
-
-ggsave(p1, file="~/test_plot1.pdf")
-
-### your turn: use the basic framework of a function and a plot that we used above to generate the standard Hardy Weinberg plot
